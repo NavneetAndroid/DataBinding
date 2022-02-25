46bbb5cb56e7ed38c96aa741e893b29cad6b827d
unknown <navneetsingh2021dec@cedcommerce.com>
2022-02-25 16:22:15 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/databinding/RetroInstance.kt
@@ -0,0 +1,20 @@
+package com.example.databinding
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetroInstance {
+
+    companion object {
+
+        val BASE_URL = "https://api.github.com/search/"
+
+        fun getRetroInstance(): Retrofit {
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/databinding/RetroService.kt
@@ -0,0 +1,9 @@
+package com.example.databinding
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RetroService {
+    @GET("repositories")
+    fun getDataFromAPI(@Query("q")query : String):Call<RecyclerList>
+}
